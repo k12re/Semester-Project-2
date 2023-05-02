def3e0eac9db7e7e168c8434cd187b74edeae6c3
Ken Thore <kenthore@live.no>
2023-05-02 15:36:41 +0200

    preliminary unauthorized fetch to test listings rendering

---
+++ src/js/api/api.mjs
@@ -0,0 +1,5 @@
+const apiUrl = "https://api.noroff.dev/api/v1";
+const auctionEndpoint = "/auction";
+const listingsEndpoint = "/listings";
+
+export const auctionListingsUrl = `${apiUrl}${auctionEndpoint}${listingsEndpoint}`;
