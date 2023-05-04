82ec72e004ff2b9c312a5a31b8c6bc2d566656db
Ken Thore <kenthore@live.no>
2023-05-04 15:40:37 +0200

    added stickybutton remover if accesstoken is in localstorage + ids on nav links

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,10 @@ import { registerFormListener } from "./listeners/register.mjs";
 import { registerUser } from "./api/register.mjs";
 import { loginFormListener } from "./listeners/login.mjs";
 import { loginUser } from "./api/login.mjs";
+
+import { load } from "./storage/storage.mjs";
+const stickyBtn = document.querySelector(".stickyBtn");
+
+if (localStorage["accessToken"]) {
+  stickyBtn.style.display = "none";
+}
