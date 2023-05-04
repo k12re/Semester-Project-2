6d43d97b89460e6a353298402b6a7cc52a5a175e
Ken Thore <kenthore@live.no>
2023-05-04 15:51:07 +0200

    removes localstorage on logoutbtn in navbar

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,22 @@ import { registerFormListener } from "./listeners/register.mjs";
 import { registerUser } from "./api/register.mjs";
 import { loginFormListener } from "./listeners/login.mjs";
 import { loginUser } from "./api/login.mjs";
+import { remove } from "./storage/storage.mjs";
 
-import { load } from "./storage/storage.mjs";
 const stickyBtn = document.querySelector(".stickyBtn");
+const logoutBtn = document.querySelector("#logoutBtn");
+console.log(logoutBtn);
 
 if (localStorage["accessToken"]) {
   stickyBtn.style.display = "none";
 }
+
+function logoutListener() {
+  logoutBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    remove("accessToken");
+    window.location.reload();
+  });
+}
+
+logoutListener();
