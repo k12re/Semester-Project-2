76e58ef994a8d52a057cee58b835b95dc30d874b
Ken Thore <kenthore@live.no>
2023-05-20 20:43:36 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,37 @@
-const path = require("path");
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default {
-  root: path.resolve(__dirname, "src"),
-  build: {
-    outDir: "../dist",
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+const publicDir = resolve(__dirname, "assets");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  css: {
+    devSourcemap: true,
   },
-  resolve: {
-    alias: {
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+  build: {
+    outDir,
+    emptyOutDir: true,
+    publicDir,
+    resolve: {
+      alias: {
+        "~bootstrap": path.resolve(dirname, "node_modules/bootstrap"),
+      },
     },
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        allListings: resolve(root, "listing/index.html"),
+        profile: resolve(root, "profile/index.html"),
+        profiles: resolve(root, "profiles/index.html"),
+      },
+    },
+    target: "esnext",
   },
   server: {
     port: 8080,
     hot: true,
   },
-};
+});
